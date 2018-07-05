d9e276962bdde310a82375bbde330f628592faaf
JomaUrbano <muurbano@up.edu.ph>
2018-07-05 16:01:28 +0800

    Automate tweet every hour

--- partyparrot.py
+++ partyparrot.py
@@ -1,13 +1,29 @@
-import requests
 import tweepy
 import credentials
+import os
+import time
+import random
 
-CONSUMER_KEY = credentials.CONSUMER_KEY
-CONSUMER_SECRET = credentials.CONSUMER_SECRET
-ACCESS_KEY = credentials.ACCESS_KEY
-ACCESS_SECRET = credentials.ACCESS_SECRET
+# APIs and connection to twitter page
+def twitter_api():
+    
+    credentials.ACCESS_KEY
+    auth = tweepy.OAuthHandler(credentials.CONSUMER_KEY, credentials.CONSUMER_SECRET)
+    auth.set_access_token(credentials.ACCESS_KEY, credentials.ACCESS_SECRET)
+    api = tweepy.API(auth)
+    return api
+
+# Randomizes a parrot from parrots folder, returns a path
+def chosenParrot():
+  parrots = os.listdir('./parrots/hd/')
+  return "./parrots/hd/{}".format(parrots[random.randint(0,len(parrots)-1)])
+
+status = ""
 
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+# Tweets a randomed parrot every hour
+while True:
+  twitter_api().update_with_media(chosenParrot(), status)
+  time.sleep(3600) 
+
+
